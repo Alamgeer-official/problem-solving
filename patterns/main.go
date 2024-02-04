51e87163310d3cc02727d789fb44cc4821292940
Alamgeer <alamgeer.official.id@gmail.com>
2024-02-04 21:44:21 +0530

    basic pattern problem solution added

---
+++ patterns/main.go
@@ -0,0 +1,111 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Patter problem solving....\n")
+	// 1 )
+	// *****
+	// *****
+	// *****
+	// *****
+	pattern1(4)
+
+	//  2)
+	// *
+	// **
+	// ***
+	// ****
+	pattern2(4)
+
+	//  3)
+	// 1
+	// 22
+	// 333
+	// 4444
+	pattern3(4)
+
+	//  4)
+	// 1
+	// 12
+	// 123
+	// 1234
+	pattern4(4)
+
+	//  5)
+	// ####
+	// ###
+	// ##
+	// #
+	pattern5(4)
+
+	//  6)
+	// 1234
+	// 123
+	// 12
+	// 1
+	pattern6(4)
+
+}
+
+func pattern1(n int) {
+	fmt.Print("\n1st problem output\n")
+	for row := 0; row < n; row++ {
+		for column := 0; column < n; column++ {
+			fmt.Printf("*")
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func pattern2(n int) {
+	fmt.Print("\n2nd problem output\n")
+	for row := 0; row < n; row++ {
+		for column := 0; column <= row; column++ {
+			fmt.Printf("*")
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func pattern3(n int) {
+	fmt.Print("\n3rd problem output\n")
+	for row := 1; row <= n; row++ {
+		for column := 1; column <= row; column++ {
+			fmt.Printf("%d", row)
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func pattern4(n int) {
+	fmt.Print("\n4th problem output\n")
+
+	for row := 1; row <= n; row++ {
+		for col := 1; col <= row; col++ {
+			fmt.Printf("%d", col)
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func pattern5(n int) {
+	fmt.Print("\n5th problem output\n")
+	for row := 0; row < n; row++ {
+		for col := 0; col < n-row; col++ {
+			fmt.Printf("#")
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func pattern6(n int) {
+	fmt.Print("\n6th problem output\n")
+	for row := 1; row <= n; row++ {
+		for col := 1; col <= n-row+1; col++ {
+			fmt.Printf("%d", col)
+		}
+		fmt.Printf("\n")
+	}
+
+}
